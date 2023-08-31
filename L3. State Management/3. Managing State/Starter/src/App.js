b7b604eca086437454feb481d0e20b4bfb692de0
Carlos Plata <carlosplata@gmail.com>
2023-08-30 20:59:26 -0500

    completed exercise state handling with useState hook

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import corgi from "./corgi.jpg";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div className="container">
-      <h2>Like this photo!</h2>
-      <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
-    </div>
-  );
+	const [likes, setLikes] = useState(0);
+
+	function countLikes() {
+		setLikes(likes + 1);
+	}
+	return (
+		<div className="container">
+			<h2>Like this photo!</h2>
+			<img src={corgi} alt="Corgi"></img>
+			<p>Amount of likes: {likes}</p>
+			<button onClick={countLikes}>
+				Click to like the picture!.
+			</button>
+			<button onClick={() => setLikes(0)}>
+				Reset likes to 0
+			</button>
+		</div>
+	);
 };
 
 export default App;
